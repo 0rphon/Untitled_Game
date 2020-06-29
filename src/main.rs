d757b3400020a4c3028ae3919893678c4263c9ed
0rphon <59403052+0rphon@users.noreply.github.com>
2020-06-29 00:07:04 -0700

    impl prototype chunk render and camera movement

--- src/main.rs
+++ src/main.rs
@@ -29,7 +29,7 @@ fn main() {
     let window = {                                                                                              //create window obj
         let size = LogicalSize::new(SCREEN_WIDTH as f64, SCREEN_HEIGHT as f64);
         WindowBuilder::new()
-            .with_title("Hello Pixels")
+            .with_title("Prototype Chunk Generation")
             .with_inner_size(size)
             .with_min_inner_size(size)
             .build(&event_loop)
@@ -44,10 +44,10 @@ fn main() {
     };
 
     let world = init_world();                                                                                   //generate world
-    let camera_coords: (isize, isize) = (0,0);                                                                  //set camera location
+    let mut camera_coords: (isize, isize) = (128,-128);                                                             //set camera location
 
     event_loop.run(move |event, _, control_flow| {                                                              //start game loop
-        //let frame_start = clock_ticks::precise_time_s();                                                        //get current loop start time
+        //let frame_start = clock_ticks::precise_time_s();                                                      //get current loop start time
         if let Event::RedrawRequested(_) = event {                                                              //if redraw requested
             draw(get_screen(&world, camera_coords), pixels.get_frame());                                        //get screen then render screen
             if pixels                                                                                           //if rendering error
@@ -59,7 +59,7 @@ fn main() {
             }                
             frames+=1;                                                                                          //inc this seconds frame counter
 
-            print!("\x1B[2J\x1B[1;1H{} FPS",fps);                                                               //print debug
+            //print!("\x1B[2J\x1B[1;1H{} FPS",fps);                                                               //print debug
             if (clock_ticks::precise_time_s() - fps_time) >= 1.0 {                                              //if second has passed since last second
                 fps = frames;                                                                                   //fps = this seconds frames
                 fps_time = clock_ticks::precise_time_s();                                                       //reset second time
@@ -76,6 +76,19 @@ fn main() {
             if input.key_pressed(VirtualKeyCode::Escape) || input.quit() {                                      //if esc pressed
                 *control_flow = ControlFlow::Exit;                                                              //exit
                 return;
+            }
+
+            if input.key_pressed(VirtualKeyCode::Up) {
+                camera_coords.1+=5;
+            }
+            if input.key_pressed(VirtualKeyCode::Down) {
+                camera_coords.1-=5;
+            }
+            if input.key_pressed(VirtualKeyCode::Left) {
+                camera_coords.0-=5;
+            }
+            if input.key_pressed(VirtualKeyCode::Right) {
+                camera_coords.0+=5;
             }
 
             if let Some(factor) = input.scale_factor_changed() {                                                //if window dimensions changed
@@ -107,6 +120,12 @@ impl Chunk {
                 data[y][x] = Cell::new((x as u8, y as u8),rgba);                                //update color
             }
         }
+        //BLACK BOX
+        for y in 0..CHUNK_HEIGHT/25 {
+            for x in 0..CHUNK_WIDTH/25 {
+                data[y][x].data.rgba = [0,0,0,0];
+            }
+        } 
         Self{                                                                                   //return instance of chunk
             chunk_coords,
             data
@@ -184,46 +203,49 @@ fn get_screen(world: &Vec<Vec<Chunk>>, camera_coords: (isize, isize)) -> Vec<[u8
 
 
 
-
 fn get_visible(world: &Vec<Vec<Chunk>>, camera_coords: (isize, isize)) -> Vec<Vec<[u8;4]>> {
     let mut screen = vec!(vec!([0;4]; SCREEN_WIDTH); SCREEN_HEIGHT);
-    let mut pixel = 0;
-    for celly in 0..world.len()*CHUNK_HEIGHT{                       //0-3
-        let ly = celly % CHUNK_HEIGHT;
-        let cy = celly / CHUNK_HEIGHT;
-        for cellx in 0..world[cy].len()*CHUNK_WIDTH {  //0-768
-            let lx = cellx % CHUNK_WIDTH;
-            let cx = cellx / CHUNK_WIDTH;
-            let wc = get_world_coords(world[cy][cx].chunk_coords, (lx as u8,ly as u8));
-            if check_visible(wc, camera_coords){
-                let px = pixel % SCREEN_WIDTH;
-                let py = pixel / SCREEN_WIDTH;
-                screen[py][px] = world[cy][cx].data[ly][lx].data.rgba;
-                pixel+=1;
-                //print!("{}\r",pixel);
+    for (gy_pos, gen_y) in world.iter().enumerate(){
+        for (gx_pos, gen_x) in gen_y.iter().enumerate(){
+            for (ly_pos, local_y) in gen_x.data.iter().enumerate(){
+                for (lx_pos, local_x) in local_y.iter().enumerate(){
+                    let wc = get_world_coords(gen_x.chunk_coords, local_x.local_coords);
+                    if check_visible(wc, camera_coords){
+                        let (sx, sy) = gen_to_screen_coords((gx_pos,gy_pos),(lx_pos,ly_pos));
+                        screen[sy][sx] = local_x.data.rgba;
+                    } 
+                }
             }
         }
     }
     screen
 }
-
+ 
 
 fn check_visible(wc: (isize, isize), camera_coords: (isize,isize)) -> bool{
     let distance_from_camera = {        
         let mut dx = wc.0 - camera_coords.0; if dx < 0 {dx = dx*-1}
-        let mut dy = wc.0 - camera_coords.1; if dy < 0 {dy = dy*-1}
+        let mut dy = wc.1 - camera_coords.1; if dy < 0 {dy = dy*-1}
         (dx as usize,dy as usize)
     };
-    if distance_from_camera.0 < SCREEN_WIDTH/2
-    && distance_from_camera.1 < SCREEN_HEIGHT/2 {true}
+    if distance_from_camera.0 <= SCREEN_WIDTH/2
+    && distance_from_camera.1 <= SCREEN_HEIGHT/2 {true}
     else {false}
 }
 
 fn get_world_coords(chunk_coords: (isize, isize), local_coords: (u8,u8)) -> (isize, isize) {
     let wx = chunk_coords.0*CHUNK_WIDTH as isize+local_coords.0 as isize;
-    let wy = chunk_coords.1* CHUNK_HEIGHT as isize+local_coords.1 as isize;
+    let wy = chunk_coords.1*CHUNK_HEIGHT as isize+local_coords.1 as isize*-1;   //i have no idea why this fixxed y axis chunk flip
     (wx,wy)
 }
+
+fn gen_to_screen_coords(gen_coords: (usize,usize), local_coords: (usize, usize)) -> (usize,usize) {
+    let screen_x = gen_coords.0*CHUNK_WIDTH + local_coords.0;
+    let screen_y = gen_coords.1*CHUNK_HEIGHT + local_coords.1;
+    (screen_x,screen_y)
+}
+
+
 
 //Chunk
 //    coords: (i32,i32),
